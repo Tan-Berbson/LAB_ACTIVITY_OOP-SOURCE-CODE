090014bd1ea54f41092ad5a4bdc21867c5434997
Tan-Berbson <berbsontan01@gmail.com>
2026-08-24 17:00:25 +0800

    Comit lang kay wala mahimo

--- Program.cs
+++ Program.cs
@@ -11,18 +11,10 @@ namespace Tan_OOPLab3
     {
       
         static void Main(string[] args)
-        {   // ang main menu class ako gitawag gamit ang method na Menu para ma display ang menu dashboard
-            // ang purpose nganong wala nako gi code deri sa program.cs  ang mga logic sa app gusto nako organize ug maintainable  akong code para dali ra matrace kung asa dapit ang error ug ma fix dayun
+        {   
             MainMenu Menumethods = new MainMenu();
             Menumethods.Menu();
-            //Methods Folder
-            // Mainmenu mao na ang dashboard 
-            // MenuCreateDisplayAction Mao na nga method gamiton para mag create ug data
-            // MenuDisplayAction Mao na nga method gamiton para sa output sa data
-            // SelectDisplay mao na nga method gamiton para sa pag select kung unsa nga data nga i display
-
-            // Models
-            // BaseClass mao na class ako gigamit sa entire app
+           
         }
     }
 }
