8101c17c7d607ccd0ef1b1ce3836fe964eaafa93
Tan-Berbson <berbsontan01@gmail.com>
2026-08-24 16:59:12 +0800

    POS Code

---
+++ Models/Pos.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tan_OOPLab3.Models
+{
+    internal class Pos :  Operations
+    {
+        private Pos product1 = new Pos("coke", 100);
+        public Pos(string name, double price) : base(name, price)
+        {
+
+        }
+
+        public void Transaction()
+        {
+            Console.WriteLine("==transaction==");
+            Console.WriteLine("[1]" + product1.Price.ToString("0.00") + product1.Name  );
+            Console.WriteLine(" ");
+
+            int choice  = Convert.ToInt32(Console.ReadLine());
+
+            Pos selectedproduct = null;
+            if(choice == 1)
+            {
+                selectedproduct = product1;
+            }
+
+            Console.WriteLine("Enter Quantity");
+            double quantity = Convert.ToDouble(Console.ReadLine());
+
+            double total = selectedproduct.Price * quantity;
+        }
+
+        //Dashboard 
+        public void Dashboard()
+        {
+            Console.WriteLine("=== POS DASHBOARD ===");
+            Console.WriteLine("[1] Manage Products");
+            Console.WriteLine("[2] View All Products");
+            Console.WriteLine("[3] Cashier Operations");
+            Console.WriteLine("[4] Exit");
+            Console.WriteLine("======================");
+
+            Console.WriteLine("Select an Options? ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                      break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    return;
+                default:
+                    Console.WriteLine("Invalid Choice");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tan_OOPLab3.Methods; 
 
 namespace Tan_OOPLab3
 {
@@ -11,18 +10,7 @@ namespace Tan_OOPLab3
     {
       
         static void Main(string[] args)
-        {   // ang main menu class ako gitawag gamit ang method na Menu para ma display ang menu dashboard
-            // ang purpose nganong wala nako gi code deri sa program.cs  ang mga logic sa app gusto nako organize ug maintainable  akong code para dali ra matrace kung asa dapit ang error ug ma fix dayun
-            MainMenu Menumethods = new MainMenu();
-            Menumethods.Menu();
-            //Methods Folder
-            // Mainmenu mao na ang dashboard 
-            // MenuCreateDisplayAction Mao na nga method gamiton para mag create ug data
-            // MenuDisplayAction Mao na nga method gamiton para sa output sa data
-            // SelectDisplay mao na nga method gamiton para sa pag select kung unsa nga data nga i display
-
-            // Models
-            // BaseClass mao na class ako gigamit sa entire app
+        {  
         }
     }
 }
