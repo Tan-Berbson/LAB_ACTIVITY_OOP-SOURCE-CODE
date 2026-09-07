23c90182d86a54571aba61f439d6d76fbd4cbd36
Tan-Berbson <berbsontan01@gmail.com>
2026-09-07 16:52:47 +0800

    feat : implement get and setters with validation

--- Models/ProductInfo.cs
+++ Models/ProductInfo.cs
@@ -10,10 +10,50 @@ namespace Tan_OOPLab3.Models
     //class
     internal class ProductInfo
     {
-        public string Name { get; set; }
-        public int Price { get; set; }
+        private string _name;
+        private int _price;
 
-        //constructor
+        // Name property
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    Console.WriteLine("Enter Product Name");
+                }
+                else
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        // Price property
+        public int Price
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    Console.WriteLine("Enter a valid Product Price");
+                }
+                else
+                {
+                    _price = value;
+                }
+            }
+        }
+
+        // Constructor
         public ProductInfo(string name, int price)
         {
             Name = name;
